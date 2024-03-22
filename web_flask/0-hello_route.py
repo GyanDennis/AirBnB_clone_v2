8b4a8a097a736793497800afca5cf4ba8a313f62
GyanDennis <126685558+GyanDennis@users.noreply.github.com>
2024-03-21 22:45:21 -0700

    updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,53 +1,24 @@
 #!/usr/bin/python3
-"""Start a flask web app
 """
+   Script to starts a Flask web application.
 
-from flask import Flask
-app = Flask(__name__)
+      The app listens on 0.0.0.0, port 5000.
+         Routes:
+                 /: Displays 'Hello HBNB!'
+                 """
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-# Define the route for route URL '/'
-@app.route('/airbnb-onepage/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Displays 'Hello HBNB!'
-    """
-    return "Hello HBNB!"
-=======
-#Define the route for the root URL '/'
-@app.route('/airbnb-onepage/', strict_slashes=False)
+                 from flask import Flask
 
-def hello hbnb():
-"""
-Displays 'Hello HBNB!'
-"""
-return "Hello HBNB!"
+                 app = Flask(__name__)
 
-if _name_ === "_main_":
->>>>>>> f6bf04063aa69be27b1e761fe97601dc17c8e3ff
 
-# Start the Flask development server
+                 @app.route("/", strict_slashes=False)
+                 def hello_hbnb():
+                     """
+                         Displays 'Hello HBNB!'
+                             """
+                                 return "Hello HBNB!"
 
-# Listen on all available network interfaces (0.0.0.0) and port 5000
-app.run(host='0.0.0.0', port=5000)
-=======
-# Define the route for the root URL '/'
-@app.route('/airbnb-onepage/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Displays 'Hello HBNB!'
-=======
 
-@app.route('/')
-def hello_flask():
-    """Return string when route queried
->>>>>>> 5cff9cdd6b7a4de7d997ac2941e4603a12e8df98
-    """
-    return 'Hello HBNB!'
-
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
->>>>>>> 858dcbfbd990ef9a145d78db078875955e4ec55d
+                                 if __name__ == "__main__":
+                                     app.run(host="0.0.0.0", port=5000)
